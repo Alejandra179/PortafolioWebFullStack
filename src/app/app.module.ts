cfdaca39e108d6cf6049f65bdc1bf85896b32cb7
Alejandra179 <alejandra.maza1@gmail.com>
2022-01-07 02:05:00 -0300

    frontend modulo2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Pagina404ComponentComponent } from './pagina404-component/pagina404-component.component';
 
+
 @NgModule({
+
   declarations: [
     AppComponent,
     Pagina404ComponentComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
